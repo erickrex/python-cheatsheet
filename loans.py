da73725755efa68e6c10b63567739ab6fc7c8317
Erick desktop <erickrhein@gmail.com>
2021-05-08 23:26:06 +0200

    student loan calculator

---
+++ loans.py
@@ -0,0 +1,20 @@
+#Get info about the loan
+money_owed = float(input("So how much do you owe?\n")) # a 50k loan gets paid in 54 months with a 3% apr and 1k payments
+annual_percentage_rate = float(input("What is the annual percentage rate? \n"))
+payment = float(input("How much can you pay monthly?"))
+forecast_payments = int(input("How many months do you want to forecast?"))
+
+#Divided by 100 to make it a percent, then divided by 12 to make it monthly
+monthly_rate = annual_percentage_rate/100/12
+
+for i in range(forecast_payments):
+    #Added interest
+    interest_paid = money_owed * monthly_rate
+    money_owed = money_owed + interest_paid
+
+    #Payments made are subtracted from the total
+    money_owed = money_owed - payment
+
+    #print the results after this month
+    print('Paid', payment, 'of which', interest_paid, 'was interest', end=' ')
+    print('Now I owe', money_owed)
