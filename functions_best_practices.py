cd4fb9614136e9e9521a88e02842bf343b8e6fba
Erick desktop <erickrhein@gmail.com>
2021-07-11 18:40:37 +0200

    Add decorators with arguments

--- functions_best_practices.py
+++ functions_best_practices.py
@@ -1,9 +1,12 @@
+import signal
 import time
+from functools import wraps
 
 
 def timer(func):
     """Define the wrapper function to return"""
     # Define the wrapper function to return
+    @wraps(func)
     def wrapper(*args, **kwargs):
         # When wrapper() is called, get the current time
         t_start = time.time()
@@ -30,6 +33,7 @@ def memoize(func):
     cache = {}
     # Define the wrapper function to return
 
+    @wraps(func)
     def wrapper(*args, **kwargs):
         # If these arguments have not been seen before
         key = (args, tuple((kwargs.items())))
@@ -52,3 +56,66 @@ print(slow_function(3, 4))
 
 print("memoized below")
 print(slow_function(3, 4))
+
+
+def raise_timeout(*args, **kwargs):
+    raise TimeoutError()
+
+
+# When an alarm signal goes off, call raise_timeout
+signal.signal(signalnum=signal.SIGALRM, handler=raise_timeout)
+
+# Set off an alarm in 5 seconds
+signal.alarm(5)
+# Cancel the alarm
+signal.alarm(0)
+
+
+def timeout_in_5s(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        # Set an alarm for 5 seconds
+        signal.alarm(5)
+        try:
+            # Call the decorated func
+            return func(*args, **kwargs)
+        finally:
+            # Cancel alarm
+            signal.alarm(0)
+    return wrapper
+
+
+@timeout_in_5s
+def foo():
+    time.sleep(10)
+    print('foo!')
+
+# DECORATOR FACTORY
+
+
+def timeout(n_seconds):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            # Set an alarm for n seconds
+            signal.alarm(n_seconds)
+            try:
+                # Call the decorated func
+                return func(*args, **kwargs)
+            finally:
+                # Cancel the alarm
+                signal.alarn(0)
+        return wrapper
+    return decorator
+
+
+@timeout(5)
+def foo():
+    time.sleep(10)
+    print('foo!')
+
+
+@timeout(20)
+def bar():
+    time.sleep(10)
+    print('bar!')
