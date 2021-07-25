7933020b03235b823377819ad4cb912c10c5e09f
Erick desktop <erickrhein@gmail.com>
2021-07-25 23:49:03 +0200

    more snippets

--- nested_context.py
+++ nested_context.py
@@ -29,3 +29,38 @@ differences = baby_names_2011.difference(baby_names_2014)
 
 # Print the differences
 print(differences)
+
+
+# Create an empty dictionary: names_by_rank
+names_by_rank = {}
+
+# Loop over the girl names
+for rank, name in female_baby_names_2012.items():
+    # Add each name to the names_by_rank dictionary using rank as the key
+    names_by_rank[rank] = name
+
+# Sort the names_by_rank dict by rank in descending order and slice the first 10 items
+for rank in sorted(names_by_rank, reverse=True)[:10]:
+    # Print each item
+    print(names_by_rank[rank])
+
+# Safely print rank 7 from the names dictionary
+print(names.get(7, 'Not found'))
+
+# Safely print the type of rank 100 from the names dictionary
+print(type(names.get(100, None)))
+
+# Safely print rank 105 from the names dictionary or 'Not Found'
+print(names.get(105, 'Not Found'))
+
+# Print a list of keys from the boy_names dictionary
+print(boy_names.keys())
+
+# Print a list of keys from the boy_names dictionary for the year 2013
+print(boy_names[2013].keys())
+
+# Loop over the dictionary
+for year in boy_names:
+    # KEY INSIDE A KEY
+    # Safely print the year and the third ranked name or 'Unknown'
+    print(year, boy_names[year].get(3, 'Unknown'))
